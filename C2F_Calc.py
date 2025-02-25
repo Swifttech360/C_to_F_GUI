4894d4388d3923b6325dde70a59d7e2e9dd4d6ba
Swifttech360 <71281505+Swifttech360@users.noreply.github.com>
2025-02-24 23:38:34 -0500

    Initial Release Finished

--- C2F_Calc.py
+++ C2F_Calc.py
@@ -1,5 +1,47 @@
+"""
+Program Name: C to F Calculator
+Author: Miles Butler
+Publish Date: 2/24/2024
+Overview:
+    This is a python program that uses a Tkinter GUI to display a program with two text boxes and a button.
+    Using the simple interface, the user can enter a Celsius temperature as a floating point number in the top entry
+    field. After hitting the "Convert" button, the program will convert the entered Cº temperature into it's
+    Fahrenheit equivalent, displaying that number in the output label.
+"""
+
+
 import tkinter as tk
+from sys import exception
 from tkinter import Button, PhotoImage
+
+def numFormatter(num):
+    """
+    :param num: A floating point number
+    :return: num, formated down to four decimal points and stripped of all trailing zeros and decimal points.
+    """
+    return f"{num:.4f}".rstrip("0").rstrip(".")
+
+def convertButtonClick():
+    """
+    Gets the Celsius input from the entry field, then converts
+    that number to Fahrenheit before displaying it in the outputLabel.
+    :return:None
+    """
+    fullTextString = entryField.get()
+    fullTextString= fullTextString.rstrip("ºC")
+    entryField.delete(0, 'end')
+    entryField.insert(0, fullTextString)
+    celsius = entryField.get()
+    try:
+        fahrenheit = 9/5 * float(celsius) + 32
+    except ValueError:
+        outputLabel.config(text="Invalid Input", foreground='#8B0000', font=('arial', 23, 'bold'))
+        print('Invalid Input\nMake sure only numbers are entered in the entry field.')
+        return None
+    fahrenheit = numFormatter(fahrenheit)
+    outputLabel.config(text=f"{fahrenheit}ºF", foreground='#ebebec', font=('arial', 20))
+    entryField.insert('end', 'ºC')
+    
 
 buttonFile = True
 
@@ -28,7 +70,7 @@ entryField.place(
     height=40,
 )
 
-entryField.insert(0, "Enter Celsius (i.e., 25)")
+entryField.insert(0, "Celsius Input (i.e., 25)")
 clicked = False
 def focusIn(event):
     global clicked
@@ -41,7 +83,7 @@ entryField.bind('<Button-1>', focusIn)
 outputLabel = tk.Label(
     foreground='#ebebec',
     text='Fahrenheit Output',
-    font=('arial', 20,),
+    font=('arial', 20),
     
 )
 outputLabel.config(background='grey')
@@ -68,7 +110,7 @@ pageLabel.place(
 )
 try:
     buttonImage = PhotoImage(
-        file='GreenButton.png')
+        file='darkGreenButton.png')
     buttonImage = buttonImage.subsample(10,12)
 except tk.TclError:
     buttonFile=False
@@ -94,17 +136,14 @@ else:
     )
     
 
-enterButton.config(text='Convert')
+enterButton.config(
+    text='Convert',
+    command=convertButtonClick
+)
 enterButton.place(
     relx=.53,
     rely=.506,
     anchor='w',
 )
-
-
-
-
-
-
 
 tk.mainloop()
